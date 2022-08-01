5bcbd437b64293a594628dcd6a2540ddf02d5f3c
Virtual Items <virtualitemsuniverse@gmail.com>
2022-08-01 14:44:43 -0500

    Create generators.py

---
+++ generators.py
@@ -0,0 +1,13 @@
+from datetime import date, datetime, timedelta
+
+def date_range(start_date: date, end_date: date, step: timedelta):
+    current_date = start_date
+    while current_date < end_date:
+        yield current_date
+        current_date += step
+
+def datetime_range(start_datetime: datetime, end_datetime: datetime, step: timedelta):
+    current_date = start_datetime
+    while current_date < end_datetime:
+        yield current_date
+        current_date += step
